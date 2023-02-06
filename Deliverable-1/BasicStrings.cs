9fa6b968a01aab69c4af8a52a484cd93a7888769
Amanda Snowden <amandasnowden@rocketmortgage.com>
2023-02-06 15:49:51 -0500

    Basic Strings and Linq Assignments

---
+++ Deliverable-1/BasicStrings.cs
@@ -0,0 +1,138 @@
+﻿using System.Diagnostics.Metrics;
+using System.Globalization;
+bool userEscape = false;
+do
+{
+    Console.WriteLine("Enter a sentence:");
+    string userSen = Console.ReadLine();
+    string[] splitResult = userSen.Split(' ');
+    for (int i = 0; i < splitResult.Length; i++)
+    {
+        Console.WriteLine(splitResult[i]);
+    }
+    Console.WriteLine("Would you like to Continue?");
+    string userRes = Console.ReadLine();
+    if (userRes == "yes" || userRes == "y")
+    {
+        userEscape = true;
+    }
+    else
+    {
+        Console.WriteLine("Thanks for playing!");
+        userEscape = false;
+    }
+} while (userEscape);
+
+Console.WriteLine("\n");// a little breaky-break
+
+bool userGetaway = false;
+do
+{
+    Console.WriteLine("Please enter some text:");
+    string userAns1 = Console.ReadLine();
+    Console.WriteLine("you have entered:" + userAns1);
+    Console.WriteLine("Would you like to continue?");
+    string userRes1 = Console.ReadLine();
+    if (userRes1 == "yes" || userRes1 == "y")
+    {
+        userGetaway = true;
+    }
+    else
+    {
+        Console.WriteLine("Goodbye!");
+        userGetaway = false;
+        break;
+    }
+    Console.WriteLine("enter some text:");
+    string userAns2 = Console.ReadLine();
+    Console.WriteLine("you have entered:" + userAns1 + " " + userAns2);
+    Console.WriteLine("Would you like to continue?");
+    string userRes2 = Console.ReadLine();
+    if (userRes2 == "yes" || userRes2 == "y")
+    {
+        userGetaway = true;
+    }
+    else
+    {
+        Console.WriteLine("Goodbye!");
+        userGetaway = false;
+        break;
+    }
+    Console.WriteLine("enter some text:");
+    string userAns3 = Console.ReadLine();
+    Console.WriteLine("you have entered:" + userAns1 + " " + userAns2 + " " + userAns3);
+    Console.WriteLine("Would you like to continue?");
+    string userRes3 = Console.ReadLine();
+    if (userRes3 == "yes" || userRes3 == "y")
+    {
+        userGetaway = true;
+    }
+    else
+    {
+        Console.WriteLine("Goodbye!");
+        userGetaway = false;
+        break;
+    }
+    Console.WriteLine("enter some text:");
+    string userAns4 = Console.ReadLine();
+    Console.WriteLine("you have entered:" + userAns1 + " " + userAns2 + " " + userAns3 + " " + userAns4);
+    Console.WriteLine("Would you like to continue?");
+    string userRes4 = Console.ReadLine();
+    if (userRes4 == "yes" || userRes4 == "y")
+    {
+        userGetaway = true;
+    }
+    else
+    {
+        Console.WriteLine("Goodbye!");
+        userGetaway = false;
+        break;
+    }
+    Console.WriteLine("enter some text:");
+    string userAns5 = Console.ReadLine();
+    Console.WriteLine("you have entered:" + userAns1 + " " + userAns2 + " " + userAns3 + " " + userAns4 + " " + userAns5);
+    Console.WriteLine("Would you like to continue?");
+    string userRes5 = Console.ReadLine();
+    if (userRes5 == "yes" || userRes5 == "y")
+    {
+        userGetaway = true;
+    }
+    else
+    {
+        Console.WriteLine("Goodbye!");
+        userGetaway = false;
+        break;
+    }
+    Console.WriteLine("enter some text:");
+    string userAns6 = Console.ReadLine();
+    Console.WriteLine("you have entered:" + userAns1 + " " + userAns2 + " " + userAns3 + " " + userAns4 + " " + userAns5 + " " + userAns6);
+    Console.WriteLine("Would you like to continue?");
+    string userRes6 = Console.ReadLine();
+    if (userRes6 == "yes" || userRes6 == "y")
+    {
+        userGetaway = true;
+    }
+    else
+    {
+        Console.WriteLine("Goodbye!");
+        userGetaway = false;
+        break;
+    }
+    Console.WriteLine("enter some text:");
+    string userAns7 = Console.ReadLine();
+    Console.WriteLine("you have entered:" + userAns1 + " " + userAns2 + " " + userAns3 + " " + userAns4 + " " + userAns5 + " " + userAns6 + " " + userAns7);
+    Console.WriteLine("Would you like to continue?");
+    string userRes7 = Console.ReadLine();
+    if (userRes7 == "yes" || userRes7 == "y")
+    {
+        Console.WriteLine("Lol sike! This is the end.");
+        userGetaway = false;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Goodbye!");
+        userGetaway = false;
+        break;
+    }
+} while (userGetaway);
---
+++ Deliverable-1/Linq1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+namespace BasicStringandLINQ
+{
+    public class Linq1
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = { 10, 2330, 112233, 12, 949, 3764, 2942, 523863 };
+
+            int result = nums.Min();
+            Console.WriteLine("Minimum Value: {0}", result); //Find the min value
+
+            int result2 = nums.Max();
+            Console.WriteLine("Maximum Value: {0}", result2); //Find the Max value
+
+            int result3 = nums.Where(x => x < 10000).Max();
+            Console.WriteLine("Maximum value less than 10k:{0}", result3); //Find the max value less than 10k
+
+            var result4 = from n in nums
+                          where (n > 10 && n < 100)
+                          select n;
+            foreach (var item in result4)
+            {
+                Console.WriteLine("The number between 10 and 100 is:" + item);
+            }//Find all the values between 10 and 100
+
+            var result5 = from n in nums
+                          where (n > 100000 && n < 999999)
+                          select n;
+            foreach (var item2 in result5)
+            {
+                Console.WriteLine(" All values between 100k and 999,999 (inclusive):" + item2);
+            }//Find all the values between 100k and 999,999 inclusive.
+
+
+            var result6 = from n in nums
+                          where (n % 2 == 0)
+                          select n;
+            foreach (var item3 in result6)
+            {
+                Console.WriteLine("All even numbers:" + item3);
+            }//Count all the even numbers.
+        }
+    }
+}
