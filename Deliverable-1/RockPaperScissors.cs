b1018969877b793e3ed36e9dd3a0428e7a57bd00
Amanda Snowden <amandasnowden@rocketmortgage.com>
2023-02-22 18:36:48 -0500

    completed Roshambo lab

---
+++ Deliverable-1/RockPaperScissors.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoshamboLab
+{
+    public enum Roshambo
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+
+    public abstract class Player
+    {
+        public string Name { get; set; }
+        public Roshambo RoshamboValue { get; set; }
+
+        public abstract Roshambo GenerateRoshambo();
+    }
+
+    public class RockPlayer : Player
+    {
+        public override Roshambo GenerateRoshambo()
+        {
+            return Roshambo.Rock;
+        }
+    }
+
+    public class HumanPlayer : Player
+    {
+        public override Roshambo GenerateRoshambo()
+        {
+            Console.WriteLine("Select your Roshambo move:");
+            Console.WriteLine("1: Rock");
+            Console.WriteLine("2: Paper");
+            Console.WriteLine("3: Scissors");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    return Roshambo.Rock;
+                case 2:
+                    return Roshambo.Paper;
+                case 3:
+                    return Roshambo.Scissors;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    return GenerateRoshambo();
+            }
+
+        }
+    }
+
+    public class RandomPlayer : Player
+    {
+        public Random random;
+
+        public RandomPlayer()
+        {
+            random = new Random();
+        }
+
+        public override Roshambo GenerateRoshambo()
+        {
+            int randomValue = random.Next(0, 3);
+            return (Roshambo)randomValue;
+        }
+    }
+
+}
---
+++ Deliverable-1/Roshambo.cs
@@ -0,0 +1,72 @@
+﻿namespace RoshamboLab
+{
+    public class RockPaperScissors
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Let's play Roshambo!");
+
+            Console.WriteLine("What is your name? ");
+            string name = Console.ReadLine();
+
+            HumanPlayer humanPlayer = new HumanPlayer();
+            humanPlayer.Name = name;
+
+            Console.WriteLine("\n");
+            Player challenger = null;
+            while (challenger == null)
+            {
+                Console.WriteLine("Please pick your opponent: RockPlayer or RandomPlayer?");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "rockplayer" || input.ToLower() == "rock")
+                {
+                    challenger = new RockPlayer();
+                }
+                else if (input.ToLower() == "randomplayer" || input.ToLower() == "random")
+                {
+                    challenger = new RandomPlayer();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid selection. Let's try again :)");
+                }
+            }
+
+            while (true)
+            {                
+                Roshambo humanChoice = humanPlayer.GenerateRoshambo();
+                Roshambo opponentChoice = challenger.GenerateRoshambo();
+
+                Console.WriteLine($"{humanPlayer.Name} chose {humanChoice}.");
+                Console.WriteLine($"{challenger.GetType().Name} chose {opponentChoice}.");
+                                
+                if (humanChoice == opponentChoice)
+                {
+                    Console.WriteLine("Tie!");
+                }
+                else if ((humanChoice == Roshambo.Rock && opponentChoice == Roshambo.Scissors) ||
+                         (humanChoice == Roshambo.Paper && opponentChoice == Roshambo.Rock) ||
+                         (humanChoice == Roshambo.Scissors && opponentChoice == Roshambo.Paper))
+                {
+                    Console.WriteLine($"{humanPlayer.Name} wins!");
+                }
+                else
+                {
+                    Console.WriteLine($"{challenger.GetType().Name} wins!");
+                }
+
+                Console.WriteLine("\n");
+                Console.WriteLine("Would you like to play again?");
+                string playAgainInput = Console.ReadLine();
+                if (playAgainInput.ToLower() != "y" || playAgainInput.ToLower() != "yes")
+                {
+                    break;
+                }
+            }
+
+        }
+    
+    }
+
+}
